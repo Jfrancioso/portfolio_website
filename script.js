1ce2da9907e9137fb1f6558355079d4ebf54a10a
Joseph Francioso <joefrancioso@gmail.com>
2023-03-17 17:50:09 -0400

    made some fixes to the personalwebsite, mars moving across screen, resume added

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ animateItems.forEach(item => {
 });
 
 const cat = document.createElement("img");
-cat.src = "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_960_720.jpg";
+cat.src = "img/MarsOnMoon.png";
 cat.style.position = "fixed";
 cat.style.width = "100px";
-cat.style.top = "50%";
-cat.style.left = "-100px";
+cat.style.top = "70px"; // adjust as needed
+cat.style.right = "20px"; // adjust as needed
 document.body.appendChild(cat);
 
 let direction = 1;
 let position = -100;
 
 function animateCat() {
-    position += direction * 2;
+    position += direction * 1;
     if (position > window.innerWidth) {
         direction = -1;
         cat.style.transform = "scaleX(-1)";
@@ -39,6 +39,28 @@ function animateCat() {
     cat.style.left = position + "px";
 }
 
+function toggleCat() {
+    cat.classList.toggle("hidden");
+}
+
+function changeCatColor() {
+    const colors = ["red", "green", "blue", "orange", "purple"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    cat.style.filter = `hue-rotate(${Math.floor(Math.random() * 360)}deg) drop-shadow(2px 2px 4px ${randomColor})`;
+}
+
+setInterval(() => {
+    animateCat();
+}, 10);
+
+setInterval(() => {
+    toggleCat();
+}, 2000);
+
+setInterval(() => {
+    changeCatColor();
+}, 3000);
+
 setInterval(animateCat, 10);
 const title = document.querySelector('.home-content h1');
 
@@ -48,4 +70,29 @@ title.addEventListener('mouseover', () => {
 
 title.addEventListener('animationend', () => {
     title.style.animation = '';
+});
+
+const menuIcon = document.querySelector('.menu-icon');
+const navMenu = document.querySelector('.nav-menu');
+
+menuIcon.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+
+/* social links highlighting */
+const contactLink = document.querySelector('#contact-link');
+const socialLinks = document.querySelectorAll('.social-links a');
+
+contactLink.addEventListener('click', (event) => {
+    event.preventDefault(); // prevent default scrolling behavior
+
+    socialLinks.forEach((link) => {
+        link.classList.add('highlight-icon', 'grow-icon');
+
+        // remove highlight and grow classes after animation completes
+        setTimeout(() => {
+            link.classList.remove('highlight-icon', 'grow-icon');
+        }, 1000);
+    });
 });
